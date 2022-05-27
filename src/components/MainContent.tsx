ccf8a1f77272985dd0b85106022c93285c97f7e0
christian-2009 <c-sophocleous@hotmail.com>
2022-05-27 18:11:35 +0100

    formatted and linted

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,109 +1,107 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import {backendURL} from '../utils/URL'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { backendURL } from "../utils/URL";
 
 interface ToDoInterface {
-    todoTitle: string;
-    todoBody: string;
+  todoTitle: string;
+  todoBody: string;
 }
 
-interface DisplayToDoListInterface{
-    todoTitle: string;
-    todoBody: string;
-    id: number
+interface DisplayToDoListInterface {
+  todo_title: string;
+  todo_body: string;
+  id: number;
 }
 
 export default function MainContent(): JSX.Element {
-    const [todoForm, setTodoForm] = useState<ToDoInterface>({
-        todoTitle: '',
-        todoBody:''
-        
-    })
-    const [displayToDoList, setDisplayToDoList] =useState<any[]>([])
-    const [toggle, setToggle] = useState<boolean>(true)
+  const [todoForm, setTodoForm] = useState<ToDoInterface>({
+    todoTitle: "",
+    todoBody: "",
+  });
+  const [displayToDoList, setDisplayToDoList] = useState<
+    DisplayToDoListInterface[]
+  >([]);
+  const [toggle, setToggle] = useState<boolean>(true);
 
-    function handleFormChange(event : (React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>)) {
-        const {name, value} = event.target
-        setTodoForm((previous) => {
-            return {... previous, [name]: value}
-        })
-    } 
+  function handleFormChange(
+    event:
+      | React.ChangeEvent<HTMLTextAreaElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) {
+    const { name, value } = event.target;
+    setTodoForm((previous) => {
+      return { ...previous, [name]: value };
+    });
+  }
 
-    
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if(todoForm.todoBody === ''){
-            window.alert('Cannot have an empty todo')
-            return
-        }
-        const response = await axios.post(backendURL + 'todo', todoForm)
-        console.log(response)
-        setTodoForm({
-        todoTitle: '',
-        todoBody:''
-        })
-        setToggle(!toggle)
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (todoForm.todoBody === "") {
+      window.alert("Cannot have an empty todo");
+      return;
     }
+    await axios.post(backendURL + "todo", todoForm);
 
-    const handleDelete = async (id: number) => {
-        const response = await axios.delete(backendURL + 'todo/' + id)
-        console.log(response)
-        setToggle(!toggle)
-    }
-    
+    setTodoForm({
+      todoTitle: "",
+      todoBody: "",
+    });
+    setToggle(!toggle);
+  };
 
-    useEffect(() => {
-        const fetchToDo = async () => {
-            const response = await axios.get(backendURL + 'todo')
-            console.log('inside function', response.data)
-            const todos = await response.data
-            setDisplayToDoList(todos)
-        }
-        fetchToDo()
-    }, [toggle])
-    console.log('outside function', displayToDoList)
+  const handleDelete = async (id: number) => {
+    const response = await axios.delete(backendURL + "todo/" + id);
+    console.log(response);
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    const fetchToDo = async () => {
+      const response = await axios.get(backendURL + "todo");
+      const todos = await response.data;
+      setDisplayToDoList(todos);
+    };
+    fetchToDo();
+  }, [toggle]);
 
   return (
     <>
-     
-     <h1>ToDoApp</h1>
-     <div className= 'todo-form'>
-     <form  onSubmit = {handleSubmit} >
-         <input 
-            onChange = {(event) => handleFormChange(event)}
-            type='text'
-            className = 'todo-form--input'
-            placeholder='title of todo...'
-            name='todoTitle'
+      <h1>ToDoApp</h1>
+      <div className="todo-form">
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(event) => handleFormChange(event)}
+            type="text"
+            className="todo-form--input"
+            placeholder="title of todo..."
+            name="todoTitle"
             value={todoForm.todoTitle}
-            id='todo-form-todoTitle'
-         />
-         <textarea
-            onChange = {(event) => handleFormChange(event)}
-            className = 'todo-form--text-area'
-            placeholder = 'type todo here...'
-            name = 'todoBody'
-            value = {todoForm.todoBody}
-            id='todo-form-todoBody'
-         />
-         <br />
-         <button>Submit</button>
-     </form>
-     </div>
+            id="todo-form-todoTitle"
+          />
+          <textarea
+            onChange={(event) => handleFormChange(event)}
+            className="todo-form--text-area"
+            placeholder="type todo here..."
+            name="todoBody"
+            value={todoForm.todoBody}
+            id="todo-form-todoBody"
+          />
+          <br />
+          <button>Submit</button>
+        </form>
+      </div>
 
-     <div>
-         <div>
-             {displayToDoList.map((obj) => (
-                 <div key = {obj.id}>
-                     <h3>{obj.todo_title}</h3>
-                     <p>{obj.todo_body}</p>
-                     <button onClick= {() => handleDelete(obj.id)}>Delete</button>
-                 </div>
-             ))}
-         </div>
-     </div>
+      <div>
+        <div>
+          {displayToDoList.map((obj) => (
+            <div key={obj.id}>
+              <h3>{obj.todo_title}</h3>
+              <p>{obj.todo_body}</p>
+              <button onClick={() => handleDelete(obj.id)}>Delete</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
-
